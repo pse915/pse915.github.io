559ac3dcda045030f121de00b864f59beac29d81
pse915 <pse915@sc.gyo6.net>
2026-09-08 23:35:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_font(size, bold=False):
     try:
         return pygame.font.Font("font.ttf", size)
     except:
-        return pygame.font.SysFont("malgungothic", size, bold=bold)
+        return pygame.font.SysFont(None, size)
 
 FONT_TITLE = get_font(24, bold=True)
 FONT_SUB = get_font(18, bold=True)
