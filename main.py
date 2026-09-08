7e673f5c7765e30677cbc470e87bfe86cf40e576
pse915 <pse915@sc.gyo6.net>
2026-09-09 00:02:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -350,7 +350,7 @@ QUESTIONS = [
 
 # ================= 구글 시트 전송 함수 (수정 완료) =================
 def submit_to_google_sheet(std_id, name, score):
-    script_url = "https://script.google.com/macros/s/AKfycbyKydQ_uEMD3jRMRXgL7UTHit1vdKQBGkakXtg-WKRqUfm_CfdcYt-xM1Ejoj8kiqhN/exec" 
+    script_url = "https://script.google.com/macros/s/AKfycbx-BTs2aPWeYtcXiBPhdbUEQWGfrYgxqD1VqDB3ZeUZZY8tWON9ZxhtGAlq9gKnc0Z0/exec" 
     data_dict = {"std_id": std_id, "name": name, "score": score}
     payload = json.dumps(data_dict).encode('utf-8')
 
